a076ed0a861e9045c99f637b0071535e2f585c44
prabhatv96 <prabhatv96@gmail.com>
2021-03-25 16:38:32 +0530

    Initial commit

---
+++ faceRecognition.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+
+package_dir = os.path.dirname(os.path.abspath(__file__))
+template = os.path.join(package_dir+'/resources', 'haarcascade_template.xml')
+faceCascade = cv2.CascadeClassifier(template)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
